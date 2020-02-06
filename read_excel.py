55ebf99466362cf3c7a87cd5eea4b4031290df4d
Hui Fang <fh2008@hotmail.com>
2020-02-06 13:14:54 +0800

    ncid raw data imported

---
+++ read_excel.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+Created on December 09 09:48:12 2019
+@author: nlog2n
+
+  预处理数据
+
+"""
+
+def read_excel_data():
+    # read from excel file
+
+    raw_data = []
+
+    import xlrd
+
+    filepath = "Workbook2.xlsx"
+    xlrd.Book.encoding = "utf8"
+    data = xlrd.open_workbook(filepath)
+
+    # 取第1张工作簿
+    table = data.sheet_by_index(0)
+    rows_count = table.nrows  # 取总行数
+    for i in range(rows_count):
+        if i == 0:
+            continue
+
+        caseno = table.cell(i, 0).value
+        lat = table.cell(i, 1).value
+        lon = table.cell(i,2).value
+        date = table.cell(i,3).value
+        age = table.cell(i,4).value
+        gender = table.cell(i,5).value
+        from_place = table.cell(i,6).value
+        staty_place = table.cell(i,7).value
+        visited_places = table.cell(i,8).value
+        related_cases = table.cell(i,9).value
+
+        caseno = caseno.strip('#')
+        caseno = int(caseno)
+        lat, lon = float(lat), float(lon)
+        age = int(age)
+
+        jsonObj =  {
+      "type": "Feature",
+      "properties": {
+        "id": caseno,
+        "osm_id": 424313400 + caseno,
+        "name": "Singapore",
+        "type": "town",
+        "z_order": 11,
+        "population": 4839400,
+        "date": date,
+        "age": age,
+        "gender": gender,
+        "from": from_place,
+        "stay": staty_place,
+        "visited": visited_places,
+        "related": related_cases
+       },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          lon,
+          lat
+        ]
+        }
+        }
+
+        raw_data.append(jsonObj)
+
+    return raw_data
+
+
+if __name__ == '__main__':
+
+    raw = read_excel_data()
+
+    geojsonObj =  {
+  "type": "FeatureCollection",
+  "crs": {
+    "type": "name",
+    "properties": {
+      "name": "urn:ogc:def:crs:OGC:1.3:CRS84"
+    }
+  },
+  "features": raw
+}
+    import json
+
+    print(json.dumps(geojsonObj))
+
+
+
